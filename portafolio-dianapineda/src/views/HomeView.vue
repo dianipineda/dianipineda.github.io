0d51da0d11d93c30f6ed332304d83a68eb3ec782
Diana <dianipineda55@gmail.com>
2025-09-09 21:42:30 -0500

    prueba cambio rutas

--- portafolio-dianapineda/src/views/HomeView.vue
+++ portafolio-dianapineda/src/views/HomeView.vue
@@ -133,14 +133,14 @@ onMounted(() => {
 
 // Secondary images for the Galleria component
 const secondaryImages = [
-  { itemImageSrc: '/python-3-logo-svgrepo-com.svg', alt: 'Python' },
-  { itemImageSrc: '/oracle-svgrepo-com.svg', alt: 'Oracle' },
-  { itemImageSrc: '/microsoft-sql-server-logo-svgrepo-com.svg', alt: 'SQL Server' },
-  { itemImageSrc: '/docker-svgrepo-com.svg', alt: 'Docker' },
-  { itemImageSrc: '/postgresql-logo-svgrepo-com.svg', alt: 'Postgresql' },
-  { itemImageSrc: '/kafka-svgrepo-com.svg', alt: 'Kafka' },
-  { itemImageSrc: '/apache-airflow-svgrepo-com.svg', alt: 'Airflow' },
-  { itemImageSrc: '/icons8-apache-spark.svg', alt: 'Spark' }
+  { itemImageSrc: 'python-3-logo-svgrepo-com.svg', alt: 'Python' },
+  { itemImageSrc: 'oracle-svgrepo-com.svg', alt: 'Oracle' },
+  { itemImageSrc: 'microsoft-sql-server-logo-svgrepo-com.svg', alt: 'SQL Server' },
+  { itemImageSrc: 'docker-svgrepo-com.svg', alt: 'Docker' },
+  { itemImageSrc: 'postgresql-logo-svgrepo-com.svg', alt: 'Postgresql' },
+  { itemImageSrc: 'kafka-svgrepo-com.svg', alt: 'Kafka' },
+  { itemImageSrc: 'apache-airflow-svgrepo-com.svg', alt: 'Airflow' },
+  { itemImageSrc: 'icons8-apache-spark.svg', alt: 'Spark' }
 ];
 </script>
 
