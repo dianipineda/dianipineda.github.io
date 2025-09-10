bfbe17c3ac418562c155b275c8a01b3ed312cf36
Diana <dianipineda55@gmail.com>
2025-09-09 22:12:01 -0500

    correcciones

--- portafolio-dianapineda/src/main.js
+++ portafolio-dianapineda/src/main.js
@@ -2,7 +2,6 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import PrimeVue from 'primevue/config';
-import './style.css';
 // Importa los estilos de PrimeVue y los íconos
 import 'primeicons/primeicons.css';
 import '@primevue/themes/aura'; // <-- Este es el estilo para los componentes
